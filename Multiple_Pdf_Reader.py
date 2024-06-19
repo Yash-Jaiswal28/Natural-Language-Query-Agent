4edecf03eebd83ada87f65ad7e2fb28f1265ae61
Yash Jaiswal <130465264+Yash-Jaiswal28@users.noreply.github.com>
2024-06-19 13:36:44 +0530

    Add files via upload

---
+++ Multiple_Pdf_Reader.py
@@ -0,0 +1,103 @@
+import streamlit as st
+from langchain.chains import LLMChain
+from langchain_community.llms import Replicate
+from langchain_core.prompts import PromptTemplate
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = "r8_Zp68LXylK2MfeWIeEkl93zhQYeh24Dn4BtlIq"
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details with the citation of it, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    llm = Replicate(
+    model="meta/meta-llama-3-8b-instruct"
+    )
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(llm, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+    # Check if the FAISS index file exists before attempting to load it
+    faiss_index_path = "faiss_index"
+    if not os.path.exists(faiss_index_path):
+        st.error(f"The FAISS index file {faiss_index_path} does not exist. Please ensure the file is correctly generated and saved.")
+        return
+
+    new_db = FAISS.load_local(faiss_index_path, embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("EMA_AI_assitant💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
+
+
+# environment - pdf_reader (conda)
